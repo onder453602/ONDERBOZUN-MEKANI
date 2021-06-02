4007cb55d2717912c6bcd2fbd380511d7b70e574
onder453602 <85020660+onder453602@users.noreply.github.com>
2021-06-02 14:19:25 +0300

    ONDERBOZUN-MEKANI

--- events/message.js
+++ events/message.js
@@ -1,43 +1,59 @@
-const config = require('../config.json')
+const ayarlar = require('../ayarlar.json');
+const db = require('quick.db')
+const Discord = require('discord.js')
+const sure = 2.5
+const beklememesaji = `**Bu Komutu Kullanabilmek İçin \`${sure}\` Saniye Kadar Beklemelisin!!**` //Komut bekleme mesaj?
+const sahipbeklemesi = false //Sahip bekleme ayar? (false=kapal?, true=aç?k)
+let yazma = new Set();
+module.exports = async message => {
 
-module.exports = async (client, message) => {
-  let prefixss = config.prefix;
-  let prefixes = [prefixss, "<@!748071702302294036>", "<@748071702302294036>"];
-  let db = require("quick.db");
-  let prefix = false;
-  for (const thisPrefix of prefixes) {
-    if (message.content.toLowerCase().startsWith(thisPrefix.toLowerCase()))
-      prefix = thisPrefix;
-  }
-
-  if (!message.content.toLowerCase().startsWith(prefix)) return;
-
-  let args = message.content
-    .slice(prefix.toLowerCase().length)
+const args = message.content
+    .slice(ayarlar.prefix.length)
     .trim()
-    .split(/ +/g);
+    .split(/ +/g);  
+  let client = message.client;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = args.shift().toLowerCase();
+if (command.length === 0) return;
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
   let cmd;
-  cmd = args.shift().toLowerCase();
-
-  let exist = db.fetch(`customcommand_${message.guild.id}_${cmd}`);
-  if (exist) {
-    message.channel.send(exist);
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+    if (yazma.has(message.author.id)) {
+      return message.channel.send(beklememesaji);
+    }
+  } else if (client.aliases.has(command)) {
+    if (yazma.has(message.author.id)) {
+      return message.channel.send(beklememesaji);
+    }
+    cmd = client.commands.get(client.aliases.get(command));
   }
-  let command;
+  if (cmd) {
 
-  if (client.commands.has(cmd)) {
-    command = client.commands.get(cmd);
-  } else if (client.aliases.has(cmd)) {
-    command = client.commands.get(client.aliases.get(cmd));
+    if(cmd.help.name !== "cevaplama") {
+     let kanalengel = await db.fetch(`kanalengel_${message.guild.id}`);
+      if(kanalengel) {
+     if(kanalengel && kanalengel.some(kanalid => `k${message.channel.id}` === kanalid)) return;
+      }
+ }
+    if (perms < cmd.conf.permLevel) return;
+    if(sahipbeklemesi === false) {
+      yazma.add(message.author.id);
+    } if(sahipbeklemesi === true) {
+      if(message.author.id === ayarlar.sahip) {
+        cmd.run(client, message, params, perms);
+        return true;
+      }
+    }
+    setTimeout(() => {
+      if(yazma.has(message.author.id)) {
+        yazma.delete(message.author.id);
+      }
+    }, sure * 1000);
+    cmd.run(client, message, params, perms);
   }
 
-  try {
-    
 
-  
-      command.run(client, message, args);
-  
-  } catch (e) {
-    console.log(e.message);
-  }
 };
